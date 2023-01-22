d5799c16f4fab5e99ae47d6689b34340d3774772
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-01-22 17:53:55 -0500

    version inicial

---
+++ src/app/loginfoview/loginfoview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginfoview',
+  templateUrl: './loginfoview.component.html',
+  styleUrls: ['./loginfoview.component.css']
+})
+export class LoginfoviewComponent {
+
+}
