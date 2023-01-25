3c2b9049e55404f03ea65299db615db4287b0537
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-01-25 10:26:36 -0500

    [Angular RxJS]

--- src/app/asyncprog/asyncprog.component.ts
+++ src/app/asyncprog/asyncprog.component.ts
@@ -33,10 +33,11 @@ export class AsyncprogComponent {
    }
    //
    RxJSTest02():void{
+      /*
       // Create an Observable that will publish a value on an interval
       const secondsCounter = interval(1000);
       // Subscribe to begin publishing values
-      const subscription = secondsCounter.subscribe(n =>
-        console.log(`It's been ${n + 1} seconds since subscribing!`));
+      const subscription   = secondsCounter.subscribe(n =>
+        console.log(`It's been ${n + 1} seconds since subscribing!`));*/
    }
 }
